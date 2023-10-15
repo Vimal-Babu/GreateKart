57d4596d4abb1cd98c0536aea85585b1066c917c
vimal <vimalpappu999@gmail.com>
2023-10-15 17:22:17 +0530

    unhide static root

--- great_cart/great_cart/settings.py
+++ great_cart/great_cart/settings.py
@@ -137,7 +137,7 @@ TIME_ZONE = 'Asia/Kolkata'  # Change this to your desired timezone
 
 STATIC_URL = 'static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
